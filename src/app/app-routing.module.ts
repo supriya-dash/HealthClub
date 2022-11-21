bb348431249b90198ae76abd3afe019ea47a1e1d
Supriya Dash <supriya.dash@happiestminds.com>
2022-11-21 12:28:38 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { PlaceAppointmentComponent } from './place-appointment/place-appointment.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"viewappointment",
+    component:ViewAppointmentComponent
+  },
+  {
+    path:"placeappointment",
+    component:PlaceAppointmentComponent
+  },
+  {
+    path:"contactus",
+    component:ContactUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
+import { PlaceAppointmentComponent } from './place-appointment/place-appointment.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewAppointmentComponent,
+    PlaceAppointmentComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
